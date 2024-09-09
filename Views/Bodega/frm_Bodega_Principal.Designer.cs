c02a4554096e50095e93e8ec12e0ea51d2a9ba84
lleroc <llleroc@yahoo.es>
2024-09-09 18:58:06 -0500

    nuevoproducto

--- Views/Bodega/frm_Bodega_Principal.Designer.cs
+++ Views/Bodega/frm_Bodega_Principal.Designer.cs
@@ -29,13 +29,46 @@ namespace Login.Views.Bodega
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 144);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Insertar Productos";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 144);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Lista de Productos";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 312);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(181, 144);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Reportes";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // frm_Bodega_Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(816, 629);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "frm_Bodega_Principal";
             this.Text = "frm_Bodega_Principal";
             this.Load += new System.EventHandler(this.frm_Bodega_Principal_Load);
@@ -44,5 +77,9 @@ namespace Login.Views.Bodega
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
---
+++ Views/Bodega/frm_nuevo_producto.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace Login.Views.Bodega
+{
+    partial class frm_nuevo_producto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbproveedor = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtcodigobarras = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtcantidad = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbunidadmedida = new System.Windows.Forms.ComboBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nuevo Producto";
+            // 
+            // cmbproveedor
+            // 
+            this.cmbproveedor.FormattingEnabled = true;
+            this.cmbproveedor.Location = new System.Drawing.Point(88, 99);
+            this.cmbproveedor.Name = "cmbproveedor";
+            this.cmbproveedor.Size = new System.Drawing.Size(258, 33);
+            this.cmbproveedor.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Proveedor";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(88, 164);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(258, 30);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtcodigobarras
+            // 
+            this.txtcodigobarras.Location = new System.Drawing.Point(88, 234);
+            this.txtcodigobarras.Name = "txtcodigobarras";
+            this.txtcodigobarras.Size = new System.Drawing.Size(258, 30);
+            this.txtcodigobarras.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(195, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nombre del Producto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Codigo de Barras";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(83, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Cantidad";
+            // 
+            // txtcantidad
+            // 
+            this.txtcantidad.Location = new System.Drawing.Point(88, 297);
+            this.txtcantidad.Name = "txtcantidad";
+            this.txtcantidad.Size = new System.Drawing.Size(258, 30);
+            this.txtcantidad.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(80, 339);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(171, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Unidad de Medida";
+            // 
+            // cmbunidadmedida
+            // 
+            this.cmbunidadmedida.FormattingEnabled = true;
+            this.cmbunidadmedida.Items.AddRange(new object[] {
+            "Unidad",
+            "Libras",
+            "Litros",
+            "Kilos",
+            "Quitales"});
+            this.cmbunidadmedida.Location = new System.Drawing.Point(88, 367);
+            this.cmbunidadmedida.Name = "cmbunidadmedida";
+            this.cmbunidadmedida.Size = new System.Drawing.Size(258, 33);
+            this.cmbunidadmedida.TabIndex = 9;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(38, 406);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(108, 72);
+            this.btnGuardar.TabIndex = 11;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(152, 406);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(108, 72);
+            this.btnCancelar.TabIndex = 12;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(266, 406);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(108, 72);
+            this.btnSalir.TabIndex = 13;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // frm_nuevo_producto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(446, 534);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmbunidadmedida);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtcantidad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtcodigobarras);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbproveedor);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frm_nuevo_producto";
+            this.Text = "Nuevo producto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbproveedor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtcodigobarras;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtcantidad;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbunidadmedida;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
